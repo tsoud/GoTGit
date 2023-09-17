7413adc2166f44c332898efb467f390a7572a923
Tamer <tamer.abousoud@gmail.com>
2023-09-17 14:25:38 -0500

    minor cleanup

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -26,14 +26,12 @@ func setupInitCommand() (*flag.FlagSet, *bool) {
 func initCmdHandler(quiet *bool) {
 	for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
 		if err := os.MkdirAll(dir, 0755); err != nil {
-			// fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
 			log.Printf("Error creating directory: %s\n", err)
 		}
 	}
 
 	headFileContents := []byte("ref: refs/heads/master\n")
 	if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
-		// fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
 		log.Printf("Error writing file: %s\n", err)
 	}
 
