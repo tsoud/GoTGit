eb716a8a63a5acbbd622c58d31c9bbe9bc559e3a
Tamer <tamer.abousoud@gmail.com>
2023-09-23 21:49:22 -0500

    Add hash-object command

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"bufio"
 	"compress/zlib"
+	"crypto/sha1"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"path"
+	"strings"
 )
 
 func setupInitCommand() (*flag.FlagSet, *bool) {
@@ -79,7 +81,66 @@ func catFileCmdHandler(pprint *bool, file string) {
 	}
 }
 
-// Usage: your_git.sh <command> <arg1> <arg2> ...
+func setupHashObjectCmd() (*flag.FlagSet, *bool, *string) {
+	hashObjCmd := flag.NewFlagSet("hash-object", flag.ExitOnError)
+
+	var write bool
+	hashObjCmd.BoolVar(&write, "w", false, "Actually write the object into the object database.")
+
+	var objType string
+	hashObjCmd.StringVar(&objType, "t", "blob",
+		"Specify the type of object to be created (default: \"blob\"). "+
+			"Possible values are `commit`, `tree`, `blob`, and `tag`.")
+
+	return hashObjCmd, &write, &objType
+}
+
+// TODO: Add functionality for handling non-blob types
+func hashObjectCmdHandler(write *bool, objType *string, file string) {
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatalf("Could not open file: %s\n", err)
+	}
+	defer f.Close()
+
+	// Read the filesize and create the header
+	info, err := f.Stat()
+	if err != nil {
+		log.Fatalf("Could not create header. Error getting file information: %s\n", err)
+	}
+	header := fmt.Sprintf("%s %d\u0000", *objType, info.Size())
+
+	// Prepend the header to the file contents and get the SHA1 sum
+	store := io.MultiReader(strings.NewReader(header), f)
+	buf, err := io.ReadAll(store)
+	if err != nil {
+		log.Fatalf("Error reading contents of %s: %s\n", file, err)
+	}
+
+	objHash := fmt.Sprintf("%x", sha1.Sum(buf))
+	fmt.Println(objHash)
+
+	if *write {
+		dstDirPath := path.Join(".git/objects", objHash[:2])
+		if err := os.MkdirAll(dstDirPath, 0755); err != nil {
+			log.Fatalf("Error creating object subdirectory in .git: %s\n", err)
+		}
+		dstFilePath := path.Join(dstDirPath, objHash[2:])
+		dst, err := os.Create(dstFilePath)
+		if err != nil {
+			log.Fatalf("Could not create object file: %s\n", err)
+		}
+		defer dst.Close()
+
+		compressed := zlib.NewWriter(dst)
+		defer compressed.Close()
+
+		if _, err := compressed.Write(buf); err != nil {
+			log.Fatalf("Could not compress object: %s\n", err)
+		}
+	}
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		log.Fatalf("\nusage: mygit <command> [<args>...]\n\n")
@@ -102,6 +163,18 @@ func main() {
 			log.Printf("More than one file given.\nOnly showing 1st file %s:", files[0])
 		}
 		catFileCmdHandler(pprint, files[0])
+
+	case "hash-object":
+		hashObjCmdArgs, write, objType := setupHashObjectCmd()
+		hashObjCmdArgs.Parse(os.Args[2:])
+		file := hashObjCmdArgs.Args()
+		if len(file) < 1 {
+			log.Fatal("No file given! Usage: mygit hash-object [-w] [-t <type>] <file>")
+		}
+		if len(file) > 1 {
+			log.Printf("Found more than one file. Only hashing %s\n", file[0])
+		}
+		hashObjectCmdHandler(write, objType, file[0])
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
